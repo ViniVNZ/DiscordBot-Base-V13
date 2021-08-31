22f077d64845d7813108257e708ad38812b33ee6
Vszn <51129029+ViniVNZ@users.noreply.github.com>
2021-08-31 04:02:36 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports.run = (client, message, args) => {
   // Excluir o comando executado pelo usuario.
-  message.delete({ timeout: 1000 });
+  message.delete();
 
   message.channel.send(`:point_right: Ping: **${parseInt(client.ws.ping)}**ms`);
 };
